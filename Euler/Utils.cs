87c3951f639af676c2f4186d7139760e27c6c58a
Steve Gagné <Steve.Gagnev4si@gmail.com>
2015-02-16 18:54:31 -0500

    Added function GetNotSumAbundants in Utils

--- Euler/Utils.cs
+++ Euler/Utils.cs
@@ -24,17 +24,48 @@ namespace Euler
         }
 
         //Returns a list containing all the abundants number up to _Last
+        //An abundant number is a number that has a higher sum of its divisors than itself.
+        //For example, 12's divisors are 1,2,3,4 and 6. The sum of these divisors is 16.
+        //16>12, thus 12 is an abundant number.
         public static List<int> GetAbundants(int _Last)
         {
             List<int> ListAbundants = new List<int>();
             for (int i = 1; i <= _Last; i++)
             {
-                if (SumDivisors(i) < i)
+                if (SumDivisors(i) > i)
                 {
                     ListAbundants.Add(i);
                 }
             }
             return ListAbundants;
         }
+
+        public static List<int> GetNotSumAbundants(List<int> ListAbundants,int _Last)
+        {
+            List<int> ListNotSum = new List<int>();
+            
+            for (int k = 1; k <= _Last; k++)
+            {
+                int i = 0;
+                int j = 0;
+                bool Sum = false;
+                while (!Sum && ListAbundants[i] < k)
+                {
+                    while (!Sum && ListAbundants[j] < k)
+                    {
+                        Sum = ListAbundants[i] + ListAbundants[j] == k;
+                        j++;
+                    }
+                    i++;
+                }
+                if (!Sum)
+                {
+                    ListNotSum.Add(k);
+                }
+            }
+            return ListNotSum;
+        }
+
+
     }
 }
