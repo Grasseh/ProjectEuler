a8d55528b9aec3bf019e3d5ae340b5752472377f
Steve Gagné <Steve.Gagnev4si@gmail.com>
2015-02-16 18:27:36 -0500

    Added Utility functions

---
+++ Euler/Utils.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Euler
+{
+    //Contains plenty of utility functions that can be reused by multiple functions.
+    static class Utils
+    {
+        //
+        public static int SumDivisors(int _Nb)
+        {
+            int Sum = 0;
+            for (int i = 1; i < _Nb; i++)
+            {
+                if (_Nb % i == 0)
+                {
+                    Sum += i;
+                }
+            }
+            return Sum;
+        }
+    }
+}
